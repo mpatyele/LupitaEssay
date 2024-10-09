8e88c9da6bc6e0a9be09efbd5a79205f2f69e5bd
Mariana Patyele <mariana01.pp@gmail.com>
2024-10-08 19:26:19 -0700

    lupita-s birthday party files added

---
+++ lupitabday/src/main/java/com/lupitabday/App.java
@@ -0,0 +1,13 @@
+package com.lupitabday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
